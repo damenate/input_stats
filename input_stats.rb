9fcaf90d3d5ca09a9fc4e4baea3260fab8996f82
Da-Me <damekim1206@gmail.com>
2016-02-01 10:19:43 -0500

    finish input stats

---
+++ input_stats.rb
@@ -0,0 +1,19 @@
+sum = 0.0
+average = 0.0
+input = 0.0
+numinput = 0.0
+
+while (input != "")
+  puts "Please enter a number."
+  input = gets.chomp
+  if input == ""
+    puts "#{sum} is your sum."
+    average = sum.to_f / numinput
+    puts "#{average} is your average."
+  elsif
+    input.to_i.to_s != input && input.to_f.to_s != input
+    else
+      sum = sum + input.to_f
+      numinput = numinput + 1
+  end
+end
