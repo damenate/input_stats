1cb889ba17e578da29139802043468d838cd4865
Da-Me <damekim1206@gmail.com>
2016-04-03 21:36:53 -0400

    update something, sorry forgot what it was

--- input_stats.rb
+++ input_stats.rb
@@ -1,19 +1,17 @@
 sum = 0.0
 average = 0.0
 input = 0.0
-numinput = 0.0
+count = 0.0
 
 while input != ""
   puts "Please enter a number."
   input = gets.chomp
   if input == ""
-    puts "#{sum} is your sum."
-    average = sum.to_f / numinput
-    puts "#{average} is your average."
-  # elsif
-  #   input.to_i.to_s != input && input.to_f.to_s != input
+    puts "#{sum} is the sum."
+    average = sum.to_f / count
+    puts "#{average} is the average."
     else
       sum = sum + input.to_f
-      numinput = numinput + 1
+      count = count + 1
   end
 end
--- input_stats_hard.rb
+++ input_stats_hard.rb
@@ -2,6 +2,7 @@ average = 0.0
 sum = 0.0
 count = 0.0
 input = 0
+
 while input != ""
   puts "Enter a number."
   input = gets.chomp
@@ -9,11 +10,11 @@ while input != ""
     puts "#{sum} is the sum of those numbers."
     average = sum / count
     puts "#{average} is the average of those numbers."
-    break
+
   elsif input.to_i.to_s != input && input.to_f.to_s != input
     puts "That is NO number."
   else
-    sum = sum+input.to_f
+    sum = sum + input.to_f
     count = count + 1
   end
 end
